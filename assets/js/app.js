336221dc26b8665493af503c3c062f1232cee120
Tijmen Wierenga <t.wierenga@live.nl>
2018-02-25 18:30:00 +0100

    Added Vue components for navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
 require('../css/app.scss');
+
+import Vue from 'vue';
+import Navigation from './components/Navigation';
+
+new Vue({
+    el: "#app",
+    components: {
+        Navigation
+    },
+    data: {
+        'menuVisible': false,
+        'windowWidth': null
+    },
+    computed: {
+        displayMenu() {
+            if (this.windowWidth > 992) return true;
+
+            return this.windowWidth < 992 && this.menuVisible;
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.getWindowWidth)
+        });
+
+        this.windowWidth = window.innerWidth;
+    },
+    methods: {
+        toggleMenu() {
+            this.menuVisible = ! this.menuVisible;
+        },
+        getWindowWidth(event) {
+            this.windowWidth = event.currentTarget.innerWidth;
+        }
+    }
+});
---
+++ assets/js/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="container mx-auto">
+        <div class="flex items-center justify-between py-2">
+            <slot name="logo"></slot>
+            <svg @click="toggleMenu" class="menu-toggle mr-4" width="32" height="32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <rect x="4" y="6" rx="1" ry="1" width="24" height="4"></rect>
+                <rect x="4" y="14" rx="1" ry="1" width="24" height="4"></rect>
+                <rect x="4" y="22" rx="1" ry="1" width="24" height="4"></rect>
+            </svg>
+        </div>
+        <div class="flex flex-grow" v-show="visible">
+            <slot name="items" v-show="visible"></slot>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        props: {
+            visible: {
+                type: Boolean,
+                required: true
+            },
+            toggleMenu: {
+                type: Function,
+                required: true
+            }
+        }
+    }
+</script>
