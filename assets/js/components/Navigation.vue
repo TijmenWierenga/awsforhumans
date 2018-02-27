2a282fbdd7ddb115e751e51c519450b1d9099645
Tijmen Wierenga <t.wierenga@live.nl>
2018-02-27 08:42:29 +0100

    Added close button to navigation for mobile

--- assets/js/components/Navigation.vue
+++ assets/js/components/Navigation.vue
@@ -2,10 +2,14 @@
     <nav class="container mx-auto" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
         <div class="flex items-center justify-between py-2">
             <slot name="logo"></slot>
-            <svg @click="toggleMenu" class="menu-toggle mr-4" width="32" height="32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <svg @click="toggleMenu" v-show="! visible" class="menu-toggle menu-toggle-open mr-4" width="32" height="32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <rect x="4" y="6" rx="1" ry="1" width="24" height="4"></rect>
                 <rect x="4" y="14" rx="1" ry="1" width="24" height="4"></rect>
                 <rect x="4" y="22" rx="1" ry="1" width="24" height="4"></rect>
+            </svg>
+            <svg @click="toggleMenu" v-show="visible" class="menu-toggle menu-toggle-close mr-4" height="32" width="32" version="1.1">
+                <line x1="7" y1="7" x2="25" y2="25" style="stroke-width:3"></line>
+                <line x1="7" y1="25" x2="25" y2="7" style="stroke-width:3"></line>
             </svg>
         </div>
         <div class="flex flex-grow" v-show="visible">
