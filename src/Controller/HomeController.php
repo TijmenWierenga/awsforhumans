c725f6d6cfedc48b9bf988e2e8c7c77d3e3cef4a
Tijmen Wierenga <t.wierenga@live.nl>
2018-03-07 09:00:26 +0100

    Added new HomeController for landingspage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace AwsForHumans\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Tijmen Wierenga <t.wierenga@live.nl>
+ */
+class HomeController extends Controller
+{
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
