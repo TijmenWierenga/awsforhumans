7e7965daab33f10c75a2a05256a038d138b63a54
Tijmen Wierenga <t.wierenga@live.nl>
2018-03-26 07:36:29 +0200

    Created controller to register subscriptions

---
+++ src/Controller/SubscriptionController.php
@@ -0,0 +1,16 @@
+<?php
+namespace AwsForHumans\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Tijmen Wierenga <t.wierenga@live.nl>
+ */
+class SubscriptionController 
+{
+    public function registerNewEmail(Request $request): Response
+    {
+
+    }
+}
